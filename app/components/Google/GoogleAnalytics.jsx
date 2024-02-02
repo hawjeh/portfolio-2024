79ff3936d2175dd7398602936052095b15053056
hawjeh <hawjeh.lee@websparks.sg>
2024-02-02 12:27:24 +0800

    remove type

--- app/components/Google/GoogleAnalytics.jsx
+++ app/components/Google/GoogleAnalytics.jsx
@@ -3,7 +3,7 @@ import Script from 'next/script';
 export default function GoogleAnalytics() {
   return (
     <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-Q071PHWZBD" type="text-javascript" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-Q071PHWZBD" />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
