88f71a2fc636981df047ada9d8626c62b5207715
hawjeh <hawjeh.lee@websparks.sg>
2024-03-21 18:16:38 +0800

    update way to calculate co2.js

--- app/components/Green/Co2CarbonEmission.jsx
+++ app/components/Green/Co2CarbonEmission.jsx
@@ -6,44 +6,40 @@ export default function Co2CarbonEmission() {
   const [emission, setEmission] = useState(0);
 
   useEffect(() => {
+    const getResourceSizes = () => {
+      const resources = [];
+
+      if (window.performance && window.performance.getEntriesByType) {
+        const entries = window.performance.getEntriesByType('resource');
+        for (const entry of entries) {
+          resources.push({
+            name: entry.name,
+            size: (entry.initiatorType === 'script' || entry.initiatorType === 'link' || entry.initiatorType === 'img' || entry.initiatorType === 'video' || entry.initiatorType === 'css') ? entry.transferSize : 0, // Estimate for scripts (might not be accurate)
+            type: entry.initiatorType, // e.g., script, img, css
+          });
+        }
+      } else {
+        console.warn('Performance API not supported in your browser.');
+      }
+
+      return resources;
+    }
+
     const calculate = () => {
       const greenHosting = true;
       const emissions = new co2({ model: "swd" });
+      const resourceSizes = getResourceSizes();
+      const size = resourceSizes.reduce((n, { size }) => n + size, 0);
 
-      if (window.performance && window.performance.getEntriesByType) {
-        var totalLoadTime = 0;
-        var totalSize = 0;
-        var perfEntries = window.performance.getEntriesByType("navigation");
-        if (perfEntries && perfEntries.length) {
-          var pageLoadTime = perfEntries[0].responseStart - perfEntries[0].loadEventEnd;
-          totalLoadTime += pageLoadTime;
+      const options = {
+        gridIntensity: {
+          device: 488.21,
+          dataCenter: { country: "SGP" },
+          network: 488.21
         }
-        var resEntries = window.performance.getEntriesByType("resource");
-        if (resEntries && resEntries.length) {
-          var resLoadTime = 0;
-          resEntries.forEach(function (resource) {
-            resLoadTime += (resource.duration);
-            totalSize += (resource.transferSize || 0);
-          })
-          totalLoadTime += resLoadTime;
-        }
-
-        // console.log("Total load time: " + totalLoadTime / 1000 + " seconds.");
-        // console.log("Total size: " + totalSize + " bytes.");
-
-        const options = {
-          gridIntensity: {
-            device: 488.21,
-            dataCenter: { country: "SGP" },
-            network: 488.21
-          }
-        };
-        const result = emissions.perByteTrace(totalSize, greenHosting, options);
-        setEmission(result.co2.toFixed(3));
-
-      } else {
-        console.log("Performance timing not supported.");
-      }
+      };
+      const result = emissions.perByteTrace(size, greenHosting, options);
+      setEmission(result.co2.toFixed(3));
     };
 
     calculate();
